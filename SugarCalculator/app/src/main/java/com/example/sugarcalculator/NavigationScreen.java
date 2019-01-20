e63ffc047cb48557487e79f5680c28bc5b7b1783
Kristen Mabry <kristen@themabryfamily.com>
2019-01-19 17:01:14 -0800

    Added search bar

--- SugarCalculator/app/src/main/java/com/example/sugarcalculator/NavigationScreen.java
+++ SugarCalculator/app/src/main/java/com/example/sugarcalculator/NavigationScreen.java
@@ -1,7 +1,9 @@
 package com.example.sugarcalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NavigationScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class NavigationScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_screen);
+    }
+
+    public void toSearch(View view) {
+        Intent intent = new Intent(this, Search.class);
+        startActivity(intent);
     }
 }
