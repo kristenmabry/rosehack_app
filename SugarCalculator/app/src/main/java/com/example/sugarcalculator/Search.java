b8cabf1ac7faea7a24a5c0fcd67fc504885657b8
Kristen Mabry <kristen@themabryfamily.com>
2019-01-19 17:55:19 -0800

    Failed api request

--- SugarCalculator/app/src/main/java/com/example/sugarcalculator/Search.java
+++ SugarCalculator/app/src/main/java/com/example/sugarcalculator/Search.java
@@ -1,7 +1,16 @@
 package com.example.sugarcalculator;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Search extends AppCompatActivity {
 
@@ -10,4 +19,60 @@ public class Search extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
     }
+
+
+
+
+    public class Search_API extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+            //progressBar.setVisibility(View.VISIBLE);
+            //responseView.setText("");
+        }
+
+        protected String doInBackground(Void... urls) {
+            TextView searchBar = findViewById(R.id.search_bar);
+            String search = searchBar.getText().toString();
+            String API_KEY = "DOy3CHZrrH9NL9yA3ogpIl2mlnnTdrT2QSRxAyFc";
+            String API_URL = "https://api.nal.usda.gov/ndb/search/?";
+            String amount = "10";
+
+            // Do some validation here
+
+            try {
+                URL url = new URL(API_URL + "q=" + search + "&apiKey=" + API_KEY + "&total=" + amount
+                                + "&format=JSON");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            Log.i("INFO", response);
+            TextView results = findViewById(R.id.results);
+            results.setText(response);
+        }
+    }
+
 }
