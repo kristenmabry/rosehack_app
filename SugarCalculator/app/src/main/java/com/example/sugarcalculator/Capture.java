c73ff251add23549f4222d597f7a7d70c99f9838
melh23 <melhenn23@gmail.com>
2019-01-19 23:06:33 -0800

    bare minimum components needed for camera setup. Still having problems.

--- SugarCalculator/app/src/main/java/com/example/sugarcalculator/Capture.java
+++ SugarCalculator/app/src/main/java/com/example/sugarcalculator/Capture.java
@@ -1,4 +1,27 @@
 package com.example.sugarcalculator;
 
-public class Capture {
+import android.content.Context;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.media.ImageReader;
+import android.support.v7.app.AppCompatActivity;
+import android.view.TextureView;
+import android.widget.Button;
+
+public class Capture extends AppCompatActivity {
+
+    private Button btnCapture;
+    private TextureView textureView;
+
+    private String cameraId;
+    private CameraDevice cameraDevice;
+    private CameraCaptureSession cameraCaptureSession;
+    private CaptureRequest.Builder captureRequestBuilder;
+    private ImageReader imageReader;
+
+    CameraManager cameraManager = Context.getSystemService(Context.CAMERA_SERVICE);
+
+
 }
