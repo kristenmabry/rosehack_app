df27d5d2b108d41aa2362e5ee9539ca979b311ec
Kristen Mabry <kristen@themabryfamily.com>
2019-01-19 21:42:27 -0800

    API working

--- SugarCalculator/app/src/main/java/com/example/sugarcalculator/Search.java
+++ SugarCalculator/app/src/main/java/com/example/sugarcalculator/Search.java
@@ -1,16 +1,22 @@
 package com.example.sugarcalculator;
 
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Search extends AppCompatActivity {
 
@@ -21,58 +27,94 @@ public class Search extends AppCompatActivity {
     }
 
 
+    public void search(View v) {
+        TextView searchBar = findViewById(R.id.search_bar);
+        String search = searchBar.getText().toString();
+        final TextView results = findViewById(R.id.name_header);
 
 
-    public class Search_API extends AsyncTask<Void, Void, String> {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
 
-        private Exception exception;
 
-        protected void onPreExecute() {
-            //progressBar.setVisibility(View.VISIBLE);
-            //responseView.setText("");
-        }
+        String API_KEY = "DOy3CHZrrH9NL9yA3ogpIl2mlnnTdrT2QSRxAyFc";
+        String API_URL = "https://api.nal.usda.gov/ndb/search/?";
+        String amount = "10";
+        String url = API_URL + "q=" + search + "&max=" + amount
+                + "&api_key=" + API_KEY;
 
-        protected String doInBackground(Void... urls) {
-            TextView searchBar = findViewById(R.id.search_bar);
-            String search = searchBar.getText().toString();
-            String API_KEY = "DOy3CHZrrH9NL9yA3ogpIl2mlnnTdrT2QSRxAyFc";
-            String API_URL = "https://api.nal.usda.gov/ndb/search/?";
-            String amount = "10";
+        // Request a string response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
-            // Do some validation here
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        JSONObject obj = new JSONObject();
+                        try {
+                            obj = response.getJSONObject("list");
 
-            try {
-                URL url = new URL(API_URL + "q=" + search + "&apiKey=" + API_KEY + "&total=" + amount
-                                + "&format=JSON");
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                try {
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(line).append("\n");
+                        }
+                        catch (JSONException e) {
+                            results.setText("Response: " + response.toString());
+                        }
+                        createRows(obj);
                     }
-                    bufferedReader.close();
-                    return stringBuilder.toString();
-                }
-                finally{
-                    urlConnection.disconnect();
-                }
-            }
-            catch(Exception e) {
-                Log.e("ERROR", e.getMessage(), e);
-                return null;
-            }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+        queue.add(jsonObjectRequest);
+    }
+
+    public void createRows(JSONObject response) {
+        TableLayout ll = findViewById(R.id.table);
+        JSONArray arr = new JSONArray();
+        try {
+            arr = response.getJSONArray("item");
+        }
+        catch (JSONException e) {
+            TextView results = findViewById(R.id.name_header);
+            results.setText("Response: " + response.toString());
         }
 
-        protected void onPostExecute(String response) {
-            if(response == null) {
-                response = "THERE WAS AN ERROR";
+
+        for (int i = 0; i < arr.length(); i++) {
+            JSONObject obj;
+            int id = 1;
+            String name = "name";
+            try {
+                obj = arr.getJSONObject(i);
+                id = Integer.parseInt(obj.getString("ndbno"));
+                name = obj.getString("name");
+
+            } catch (JSONException e) {
+                obj = new JSONObject();
             }
-            Log.i("INFO", response);
-            TextView results = findViewById(R.id.results);
-            results.setText(response);
+            TableRow tr = new TableRow(this);
+            tr.setId(id);
+
+            if (i % 2 == 0) {
+                tr.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+            }
+            else {
+                tr.setBackgroundColor(getResources().getColor(R.color.colorBackground));
+            }
+
+
+            TextView label = new TextView(this);
+            label.setText(name);
+
+            tr.addView(label);
+            ll.addView(tr, i+1);
         }
+
     }
+
 
 }
